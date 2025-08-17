94296bb989ad6724304e92353e2fa7a477b73c49
Rishi Raj <121712001+rishi2909@users.noreply.github.com>
2025-08-17 16:32:11 +0530

    Update Digits_Sum_Palindrom.java

--- Digits_Sum_Palindrom.java
+++ Digits_Sum_Palindrom.java
@@ -1,12 +1,4 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
-class Solution {
+ class Solution {
     public static boolean isDigitSumPalindrome(int n) {
         int sum = 0;
         int temp = n;
